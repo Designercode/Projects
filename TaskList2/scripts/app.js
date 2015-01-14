ed26ece2b81b05c6063ef5b6d7b5f2189450f410
Designercode <aprilcummings@Aprils-MBP.fritz.box>
2015-01-14 20:46:00 +0100

    ready for input

--- TaskList2/scripts/app.js
+++ TaskList2/scripts/app.js
@@ -8,25 +8,25 @@
 
    tasks: 
    [
-      { task: 'laundry', 
+      { taskName: 'laundry', 
         dateCreated: '12.16.2014', 
         importance: 3,
         status: 'active' 
       },
 
-      { task: 'wash dishes', 
+      { taskName: 'wash dishes', 
         dateCreated: '12.05.2014', 
         importance: 2,
         status: 'expired' 
       },
       
-      { task: 'grocery shopping', 
+      { taskName: 'grocery shopping', 
         dateCreated: '12.15.2014', 
         importance: 4,
         status: 'completed' 
       },
       
-      { task: 'send postcard', 
+      { taskName: 'send postcard', 
         dateCreated: '12.10.2014', 
         importance: 3,
         status: 'active'
@@ -54,14 +54,78 @@
       });
    }]);
 
+// Populates Task List with Given Object Array
+
  BlocItOff.controller('activeTasks.controller', ['$scope', function($scope) 
   {
     $scope.tasks = [];
-      for (var i = 0; i <6; i++)
+      for (var i = 0; i<TaskList.tasks.length; i++)
       {
-      $scope.tasks.push(angular.copy(TaskList)); 
-      }   
+        $scope.tasks.push(angular.copy(TaskList.tasks[i])); 
+      }
+
+
+    // Controlling Priority Icons
+
+    $scope.fillStar = function(timesClicked)
+    {
+      if((timesClicked %2) !== 0) //Odd Number of Clicks
+      {
+        console.log('true');
+        return true;
+      }
+      
+      else if((timesClicked %2) === 0) //Even Number of Clicks
+      {
+        console.log('false');
+        return false;
+      }
+    } 
+
+   $scope.starClass = function(fillStar)
+    {
+      console.log(fillStar);
+      if(fillStar === true)
+      {
+        console.log('star');
+        return "fa-star";
+      }
+
+      else if(fillStar !== true)
+      {
+        console.log('outline');
+        return "fa-star-o";
+      }          
+    }
+
+    //Adds New Task to TaskList on Form Submit
+    $scope.addTask = function()
+    {
+    $scope.tasks.push({taskName:$scope.enterTaskName, status: "active"});
+    }
+
   }]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
