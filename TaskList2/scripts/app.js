fd65e4c70655b162b16b5a1a102bacdbb91716f4
Designercode <aprilcummings@Aprils-MBP.fritz.box>
2015-01-14 20:51:12 +0100

    how to access Object

--- TaskList2/scripts/app.js
+++ TaskList2/scripts/app.js
@@ -11,19 +11,19 @@
       { taskName: 'laundry', 
         dateCreated: '12.16.2014', 
         importance: 3,
-        status: 'active' 
+        status: 'active'
       },
 
       { taskName: 'wash dishes', 
         dateCreated: '12.05.2014', 
         importance: 2,
-        status: 'expired' 
+        status: 'expired'
       },
       
       { taskName: 'grocery shopping', 
         dateCreated: '12.15.2014', 
         importance: 4,
-        status: 'completed' 
+        status: 'completed'
       },
       
       { taskName: 'send postcard', 
@@ -65,46 +65,158 @@
       }
 
 
-    // Controlling Priority Icons
+    // Controlling Priority Icons by Toggle on Click: Step 1 retrieve element id.
+    var currentStar = null;
+    var Imp = null;
+    var newStatus = null;
 
-    $scope.fillStar = function(timesClicked)
+    $scope.fillStar = function(event)
     {
-      if((timesClicked %2) !== 0) //Odd Number of Clicks
-      {
-        console.log('true');
-        return true;
-      }
-      
-      else if((timesClicked %2) === 0) //Even Number of Clicks
-      {
-        console.log('false');
-        return false;
-      }
+      currentStar = event.target.id;
     } 
 
-   $scope.starClass = function(fillStar)
+   
+   // using the id of the star to determine which icon state is activated
+   $scope.starClass = function()
     {
-      console.log(fillStar);
-      if(fillStar === true)
-      {
-        console.log('star');
-        return "fa-star";
-      }
+      
+     if(currentStar === "star1")
+        {
+          document.getElementById("star1").className = "fa fa-star";
+          document.getElementById("star2").className = "fa fa-star-o";
+          document.getElementById("star3").className = "fa fa-star-o";
+          document.getElementById("star4").className = "fa fa-star-o";
+          document.getElementById("star5").className = "fa fa-star-o";
+        }
 
-      else if(fillStar !== true)
-      {
-        console.log('outline');
-        return "fa-star-o";
-      }          
+     else if(currentStar === "star2")
+        {
+          document.getElementById("star1").className = "fa fa-star";
+          document.getElementById("star2").className = "fa fa-star";
+          document.getElementById("star3").className = "fa fa-star-o";
+          document.getElementById("star4").className = "fa fa-star-o";
+          document.getElementById("star5").className = "fa fa-star-o";
+        }
+
+     else if(currentStar === "star3")
+        {
+          document.getElementById("star1").className = "fa fa-star";
+          document.getElementById("star2").className = "fa fa-star";
+          document.getElementById("star3").className = "fa fa-star";
+          document.getElementById("star4").className = "fa fa-star-o";
+          document.getElementById("star5").className = "fa fa-star-o";
+        }
+
+     else if(currentStar === "star4")
+        {
+          document.getElementById("star1").className = "fa fa-star";
+          document.getElementById("star2").className = "fa fa-star";
+          document.getElementById("star3").className = "fa fa-star";
+          document.getElementById("star4").className = "fa fa-star";
+          document.getElementById("star5").className = "fa fa-star-o";
+        }
+
+    else if(currentStar === "star5")
+        {
+          document.getElementById("star1").className = "fa fa-star";
+          document.getElementById("star2").className = "fa fa-star";
+          document.getElementById("star3").className = "fa fa-star";
+          document.getElementById("star4").className = "fa fa-star";
+          document.getElementById("star5").className = "fa fa-star";
+        }
+
+    else
+        {
+          return "fa-star-o";
+        }
+
+    // records which stars have been activated by setting the "importance number"
+    if (document.getElementById(currentStar).className = "fa fa-star");
+        {
+          Imp = currentStar.replace( /[^\d.]/g, '' ); // This works: "importance" is later set to the number found within the star id. 
+        }
+
     }
 
-    //Adds New Task to TaskList on Form Submit
+    // controlling task status -> add 7-day expiration function in here later
+    $scope.getTaskStatus = function(event)
+    {
+      if (document.getElementById("check").checked === true)
+      {
+        newStatus = "completed";
+      } 
+
+      else if (document.getElementById("check").checked === false)
+      {
+        newStatus = "active";
+      }
+    }
+
+
+    // ! Push new status to exisiting Task... How to access & update pre-existing tasks?
+
+
+    // Adds New Task to TaskList on Form Submit
     $scope.addTask = function()
     {
-    $scope.tasks.push({taskName:$scope.enterTaskName, status: "active"});
+    $scope.tasks.push({taskName:$scope.enterTaskName, importance: Imp, status: "active"});
     }
 
+
+     // ! Populate stars (on created tasks) depending on the importance: number... How to access the "importance" property in Tasks.tasks[i]?
+    // Naturally, I would have prefered to re-use the if statements above, but couldn't think of a way to do it...
+    
+    // I know the code below is wrong, but I wanted to show the concept...
+
+    /*
+        $scope.populateTaskStars = function()
+        {
+            switch($scope.task.importance) 
+            {
+
+                              case 1:
+                                      document.getElementById("imp1").className = "fa fa-star";
+                                      document.getElementById("imp2").className = "fa fa-star-o";
+                                      document.getElementById("imp3").className = "fa fa-star-o";
+                                      document.getElementById("imp4").className = "fa fa-star-o";
+                                      document.getElementById("imp5").className = "fa fa-star-o";
+                                      break;
+                              case 2:
+                                      code block...
+                                      break;
+                              case 3:
+                                      code block...
+                                      break;
+                              case 4:
+                                      code block...
+                                      break;
+                              case 5:
+                                      code block...
+                                      break;        
+                              default:
+                                      default return "fa-star-o";
+
+            }
+          }
+    */
+
+    // control which tasks are shown on the active page
+
+/*
+    $.scope.hidden = function()
+    {
+      if (task.status !== "active")
+      {
+        return true;
+      }
+    }
+
+*/
+
   }]);
+
+
+   
 
 
 
